9f43880f771c8a581b67d3646b510ca7998e11c9
Dennis Bell <dennis.j.bell@gmail.com>
2018-12-11 15:59:59 -0800

    Document Genesis::Vault library

--- lib/Genesis/Vault.pm
+++ lib/Genesis/Vault.pm
@@ -409,24 +409,246 @@ This module provides utilities for interacting with a Vault through safe.
 
 Returns a blessed Genesis::Vault object based on the URL, target name and TLS verify values provided.
 
-B<NOTE:> This should not be called directly, as it provides no
+B<NOTE:> This should not be called directly, as it provides no error checking or validations.
 
-=head2 target($url)
+=head2 target($target, %opts)
 
-Returns a C<Genesis::Vault> object representing the vault at the given target.
+Returns a C<Genesis::Vault> object representing the vault at the given target
+or presents the user with an interactive prompt to specify a target.  This is
+intended to be used when setting up a deployment repo for the first time, or
+selecting a new vault for an existing deployment repo.
 
-This will raise an exception if this vault is not found locally
-Reads the contents of C<$path>, interprets it as YAML, and parses it into a
-Perl hashref structure.  This leverages C<spruce>, so it can only be used on
-YAML documents with top-level maps.  In practice, this limitation is hardly
-a problem.
+In the case that the target is passed in, the target will be validated to
+ensure that it is known, a url or alias and that its url is unique (not being
+used by any other aliases); A C<Genesis::Vault> object for that target is
+returned if it is valid, otherwise, an error will be raised.
 
-=head2 load($yaml)
+In the case that the target is not passed in, all unique-url aliases will be
+presented for selection, with the current system target being shown as a
+default selection.  If there are aliases that share urls, a warning will be
+presented to the user that some invalid targets are not shown due to that.
+The user then enters the number corresponding to the desired target, and a
+C<Genesis::Vault> object corresponding to that slection is returned.  This
+requires that the caller is in a controlling terminal, otherwise the program
+will terminate.
 
-Interprets its argument as a string of YAML, and parses it into a Perl
-hashref structure.  This leverages C<spruce>, so it can only be used on
-YAML documents with top-level maps.  In practice, this limitation is hardly
-a problem.
+C<%opts> can be the following values:
+
+=over
+
+=item default_vault
+
+A C<Genesis::Vault> that will be used as the default
+vault selection in the interactive prompt.  If not provided, the current system
+target vault will be used.  Has no effect when not in interactive mode.
+
+=back
+
+In either cases, the target will be validated that it is reachable, authorized
+and ready to be used, and will set that vault as the C<current> vault for the
+class.
+
+=head2 attach($url, $insecure)
+
+Returns a C<Genesis::Vault> object for the given url according to the user's
+.saferc file.
+
+This will result in an error if the url is not known in the .saferc or if it
+is not unique to a single alias, as well as if the url is not a valid url.
+
+The C<insecure> does not matter for the attach, but does change the error
+output for describing how to add the target to the local safe configuration if
+it is missing.
+
+=head2 rebind
+
+This is used to rebind to the previous vault when in a callback from a Genesis-
+run hook.  It uses the C<GENESIS_TARGET_VAULT> environment variable that is set
+prior to running a hook, and only ensures that the vault is known to the system.
+
+=head2 find(%conditions)
+
+Without any conditions, this will return all system-defined safe targets as
+Genesis::Vault objects.  Specifying hash elemements of the property => value
+filters the selection to those that have that property value (compared as string)
+Valid properties are C<url>, C<name>, C<tls> and C<verify>.
+
+=head2 find_by_target($alias_or_url)
+
+This will return all Vaults that use the same url as the given alias or url.
+
+=head2 default
+
+This will return the Vault that is the set target of the system, or null if
+there is no current system target.
+
+=head2 current
+
+This will return the Vault that was the last Vault targeted by Genesis::Vault
+methods of target, attach or rebind, or by the explicit set_as_current method
+on a Vault object.
+
+=head2 clear_all
+
+This method removes all cached Vault objects and the C<current> and C<default>
+values.  Though mainly used for providing a clean slate for testing, it could
+also be useful if the system's safe configuration changes and those changes need
+to be picked up by Genesis during a run.
+
+=head1 Instance Methods
+
+Each C<Genesis::Vault> object is composed of the properties of url, its name
+(alias) as it is known on the local system, and its verify (binary opposite of
+skip-ssl-validation).  While these properties can be queried directly, it is
+better to use the accessor methods by the same name
+
+=head2 url
+
+Returns the url for the Vault object, in the form of:
+C<schema://host_name_or_ip:port>
+
+The :port is optional, and is understood to be 80 for http schema or 443 for
+https.
+
+=head2 name
+
+Returns the name (aka alias) of the vault as it is known on the local system.
+Because the same Vault target url may be known by a different name on each
+system, the use of the alias is not considered an precise identifier for a
+Vault, and only used for convenience in display output or specifying a target
+initially.
+
+=head2 verify
+
+Returns a boolean true if the vault target's certificate will be validated
+when it is connected, or false if not.  Only applicable to https urls, though
+http will default to true.
+
+=head2 tls
+
+Convenience method to check if using https (true) or http (false) rather than
+having to substring or regex the url.
+
+=head2 query
+
+Allows caller to pass a generic query to the selected vault.  The user can
+specify anything that would normally come after `safe ...` on the command line,
+but not the -T <target> option will NOT have any effect.
+
+This can take the same arguments and returns the same structure that a
+C<Genesis::run> method would, with two caveats:
+
+=over
+
+=item *
+
+Setting the environment variable SAFE_TARGET will get overwritten with the url
+of the Vault object being operated on.
+
+=item *
+
+Setting the DEBUG environment variable will get unset because it is disruptive
+to the call.  If you want to see the call being made so you can debug it, run
+the Genesis command with -T or set the GENESIS_TRACE variable to 1
+
+=back
+
+=head2 get($path[, $key])
+
+Return the string of the given path and key, or return the entire content under
+the given path if no key is given.  The path does not have to be an end node
+that contains keys; it can be a branch path, in which case all the sub-paths
+and their key:value pairs will be returned.
+
+=head2 set($path, $key[, $value])
+
+If a value is specified, it will set that value (as a string) to the given key
+on the specified path.  If no value is provided, an interactive mode will be
+started where the user will be prompted to enter the value.  This will be
+'dotted' out on the screen, and the user will have to enter the same value
+again to confirm the correctness of their entry.
+
+=head2 has($path[, $key])
+
+Returns true if the vault contains the path and optionally the key if given.
+Equivalent to C<safe exists $path> or C<safe exists $path:$key> as appropriate.
+
+=head2 paths([@prefixes])
+
+Returns a list of all paths in the vault if no prefix was specified, or all
+paths that can be found under the specified prefixes.  If you ask for
+overlapping prefixes, paths that match multiple prefixes will be returned
+multiple times.
+
+Note that this will only return node paths (paths that contain keys on their
+last path segment, so if a vault only contains
+B<secret/this/is/my/long/path:key> and you asked for paths, it would only
+return that entry, not each partial path.
+
+=head2 keys
+
+Similar to C<paths> above, but also includes the B<:key> suffix for each key
+under the matching paths.
+
+=head2 status
+
+Returns the status of the vault.  This is a string value that can be one of the
+following:
+
+=over
+
+=item unreachable
+
+This means that the vault url or port is not responding to connection attempts.
+This may be because the C<vault> executable has stopped working, or due to
+networking issue (e.g.: VPN not connected)
+
+=item unauthenticated
+
+This means that the vault is responding, but the local safe token has expired
+or not been set.  Run C<safe auth ...> to connect, then try the command again.
+
+=item sealed
+
+The vault is sealed, and must be unsealed by the administrator before you can
+access it.
+
+=item uninitialized
+
+The vault is responding and authenticated, but does not look like it was
+correctly initialized with safe.
+
+This may be a basic vault that was stood up manually -- to resolve this, simply
+run `safe set secret/handshake knock=knock` once you're sure your talking to
+the correct vault
+
+=item ok
+
+The vault is operating normally and the user is authenticated.
+
+=back
+
+=head2 env
+
+This returns a hash of the environment variable names and values for
+configuring the vault for things that use the basic Hashicorp vault environment
+variables to target a vault, such as C<spruce>.  This can be fed directly into
+the C<Genesis::run> commands C<env> option.
+
+=head2 token
+
+The authentication token for the vault, as stored in the C<.saferc> file.
+
+=head2 set_as_current
+
+Set the vault object as the current vault object used by this run of Genesis.
+This is sometimes needed when dealing with legacy aspects of genesis
+(pipelines, params from kit.yml) where there is no passing in of the C<Env> or
+C<Top> object.
+
+This is automatically called by C<target>, C<attach> and C<rebind> and
+generally doesn't need to be manually set, but there are a few circumstances
+that it may be necessary, so this was exposed as a public method.
 
 =cut
 
