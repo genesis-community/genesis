16f0cb37d3d499c0c61a62b048e524f66acc4515
Dennis J. Bell <dennis.j.bell@gmail.com>
2020-03-26 19:34:22 -0700

    Add support for bosh variables (#385)
    In order to fill in variables referenced in double-parentheses, users
    can add them under `params.bosh-variables` in their env files, instead
    of having to have them in separate files.  Kits can also specify default
    values for these variables, something that is not supported by BOSH
    directly.

--- lib/Genesis/IO.pm
+++ lib/Genesis/IO.pm
@@ -2,7 +2,7 @@ package Genesis::IO;
 
 use base 'Exporter';
 our @EXPORT = qw/
-	DumpJSON LoadFile Load
+	DumpJSON DumpYAML LoadFile Load
 /;
 
 use JSON::PP qw/decode_json encode_json/;
@@ -13,6 +13,14 @@ sub DumpJSON {
 	open my $fh, ">", $file or die "Unable to write to $file: $!\n";
 	print $fh encode_json($data);
 	close $fh;
+}
+
+sub DumpYAML {
+	my ($file, $data) = @_;
+	my $i=1; while (-f "$file.$i.json") {$i++};
+	my $tmpfile = "$file.$i.json";
+	DumpJSON($tmpfile,$data);
+	run('cat "$1" | spruce merge - > $2; rm "$1"', $tmpfile, $file);
 }
 
 sub LoadFile {
