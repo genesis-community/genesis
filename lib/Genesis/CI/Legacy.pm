beab4e4c78a46a06c90915263be48281e81c98c4
Benjamin Gandon <benjamin@gstack.io>
2022-11-10 20:23:51 +0100

    No need for the BOSH_ENVIRONMENT env var + Remove letfover checks in 'genesis ci-pipeline-run-errand' implementation

--- lib/Genesis/CI/Legacy.pm
+++ lib/Genesis/CI/Legacy.pm
@@ -1357,13 +1357,6 @@ EOF
 			if $pipeline->{pipeline}{vault}{'no-strongbox'};
 		print $OUT "            VAULT_NAMESPACE:      $pipeline->{pipeline}{vault}{namespace}\n"
 			if $pipeline->{pipeline}{vault}{namespace};
-
-		# don't supply bosh creds if we're create-env, because no one to talk to
-		unless ($E->use_create_env) {
-			print $OUT <<EOF;
-            BOSH_ENVIRONMENT:     $pipeline->{pipeline}{boshes}{$env}{url}
-EOF
-		}
 		print $OUT <<EOF if $pipeline->{pipeline}{debug};
             DEBUG:                $pipeline->{pipeline}{debug}
 EOF
@@ -1498,13 +1491,6 @@ EOF
 			if $pipeline->{pipeline}{vault}{'no-strongbox'};
 		print $OUT "            VAULT_NAMESPACE:      $pipeline->{pipeline}{vault}{namespace}\n"
 			if $pipeline->{pipeline}{vault}{namespace};
-
-		# don't supply bosh creds if we're create-env, because no one to talk to
-		unless ($E->use_create_env) {
-			print $OUT <<EOF;
-            BOSH_ENVIRONMENT:     $pipeline->{pipeline}{boshes}{$env}{url}
-EOF
-		}
 		print $OUT <<EOF if $pipeline->{pipeline}{debug};
             DEBUG:                $pipeline->{pipeline}{debug}
 EOF
@@ -1559,8 +1545,6 @@ ${registry_creds}
             CI_NO_REDACT:         $pipeline->{pipeline}{unredacted}
             CURRENT_ENV:          $env
             ERRAND_NAME:          $errand_name
-
-            BOSH_ENVIRONMENT:     $pipeline->{pipeline}{boshes}{$env}{url}
 EOF
 			print $OUT <<EOF if $pipeline->{pipeline}{debug};
             DEBUG:                $pipeline->{pipeline}{debug}
