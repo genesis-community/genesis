a5fc359472478da39a18f09f5ae2bd854cacf4d0
Wayne E. Seguin <wayneeseguin@gmail.com>
2026-05-25 10:24:57 -0400

    Adding pve stemcell prefix

--- lib/Service/BOSH/Stemcell.pm
+++ lib/Service/BOSH/Stemcell.pm
@@ -186,6 +186,7 @@ sub cpi_stemcell_prefix {
     'google'      => 'google-kvm',
     'openstack'   => 'openstack-kvm',
 		'stackit'     => 'openstack-kvm',
+    'pve'         => 'openstack-kvm',
     'vsphere'     => 'vsphere-esxi',
     'virtualbox'  => 'warden-boshlite',
     'warden'      => 'warden-boshlite',
